2fa5a743847d0385125d7cb11e6d90dd4aa75667
tanxx22 <113966865+tanxx22@users.noreply.github.com>
2026-07-03 17:04:50 +0800

    fix text splitter import

--- src/ingest.py
+++ src/ingest.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
